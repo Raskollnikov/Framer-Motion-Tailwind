2821817d179750e245263c34c2e8629188e3c6f1
Raskollnikov <bakhalaia56@gmail.com>
2025-01-12 01:41:07 +0400

    initialize app + add important libraries + fonts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+function App() {
+
+
+  return (
+    <div className='font-primary bg-heroBg text-white overflow-x-hidden h-screen flex items-center justify-center'>
+      </div>
+  )
+}
+
+export default App
